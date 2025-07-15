3872236c5bbdd63f07c8e766c5fae7154f31381e
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-16 01:46:05 +0700

    fix

--- src/pages/PhotoList.tsx
+++ src/pages/PhotoList.tsx
@@ -180,7 +180,7 @@ export default function PhotoList() {
                         aria-label="Chat di WhatsApp"
                       >
                         <FaWhatsapp className="w-5 h-5 mr-2" />
-                        Chat WhatsApp
+                        Chat
                       </button>
                     </div>
                   </div>
