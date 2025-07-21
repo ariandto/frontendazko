3d1128231202cca7b8612fd6a90b79d020d534e4
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-21 08:34:40 +0700

    disable isActive fitur

--- src/pages/PhotoList.tsx
+++ src/pages/PhotoList.tsx
@@ -198,7 +198,7 @@ export default function PhotoList() {
                               : "bg-gray-400 text-white/70 cursor-not-allowed"
                           }`}
                           onClick={() => isActive && openWhatsApp(user.phone)}
-                          disabled={!isActive}
+{/*                           disabled={!isActive} */}
                         >
                           <FaWhatsapp className="w-5 h-5 mr-2" />
                           Chat
