4eb3169f2a3f110424e3dbba6ee0d52a82259e0d
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-23 15:18:11 +0700

    update to gray

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -25,7 +25,7 @@ export default function Navigation() {
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center gap-8">
-            <Link to="/listpic" className="flex items-center gap-1 hover:text-orange-600 font-semibold hover:underline">
+            <Link to="/listpic" className="flex items-center gap-1 hover:text-gray-500 font-semibold hover:underline">
               <Users className="w-5 h-5" /> PIC
             </Link>
             <Link to="/form-request" className="flex items-center gap-1 hover:text-blue-500 font-medium transition-all">
