1502bf50beb91f15e75360a2966c2bf090569ae0
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-31 15:15:45 +0700

    Update apitracking.tsx

--- src/config/apitracking.tsx
+++ src/config/apitracking.tsx
@@ -1,10 +1,6 @@
 const apiMap: Record<string, string> = {
   "2025-07": "https://script.google.com/macros/s/AKfycboxOlaX9sXLv81EfWfOqULepP4HtrUCQF1e1vzai2h8c8TK34C62B9WhNy5X0YjdovPA/exec",
   "2025-08": "https://script.google.com/macros/s/AKfycbyLsCVNK47PozDxWpDg4LUZG3n0xDbGs-MMBC5rjT6bAlYlCF6XK4iuYmFzwl6wA6bbTw/exec",
-  // tinggal tambahkan:
-  // "2025-09": "...",
-  // "2025-10": "...",
-  // dst.
 };
 
 export function getTrackingApi(): string {
