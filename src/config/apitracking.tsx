f408dba9876a470d52c339c147e99988f2ad37f0
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-31 14:43:37 +0700

    Update and rename apitracking.txt to apitracking.tsx

--- src/config/apitracking.tsx
+++ src/config/apitracking.tsx
@@ -2,4 +2,4 @@
 https://script.google.com/macros/s/AKfycbxOlax9sX3LV8iEWFOqLUeP4HrtUCQElr1ev2aizh8c8IKJ4C62Bf9WmNy5X9IydJovPA/exec
 
 //api tacking agustus
-coming soon
+https://script.google.com/macros/s/AKfycbyh85HaM3clZbei79VAADL7aHiqNvtsFeuAisKz2OAFaVS1cTGNTgzBGIlMF-5JibjlxQ/exec
