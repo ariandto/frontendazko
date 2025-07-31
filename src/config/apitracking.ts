6ece7b280e2d42c132e333cd039d9a7b60cd6ccd
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-31 17:36:47 +0700

    update

--- src/config/apitracking.ts
+++ src/config/apitracking.ts
@@ -1,6 +1,6 @@
 const apiMap: Record<string, string> = {
   "2025-07": "https://script.google.com/macros/s/AKfycby-nn0mM3hJw5x_trEgl9ao6d6hFmwby9GA8Qcuk8zScrks3vsJThSLqoGu7EkN_UhXrQ/exec",
-  "2025-08": "https://script.google.com/macros/s/AKfycbzIQL_D4aK1X4KciDkgQX-4I4Cy0bQKGKm_ie2fV4qfPSijjH4ON-T8TGHJXUL7z62-fQ/exec",
+  "2025-08": "https://script.google.com/macros/s/AKfycbxl0jdwaQtk7-xKHtZlD14vRXPkDJdzUrvF1MUr-xs-cNX6m6EDfpZi_tzNuPRCS3iW9Q/exec",
 };
 
 export function getTrackingApi(): string {
