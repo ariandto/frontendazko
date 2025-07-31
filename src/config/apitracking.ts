659445c29931420069d63ce76d18bd961e8219d3
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-31 16:31:07 +0700

    up agustus

--- src/config/apitracking.ts
+++ src/config/apitracking.ts
@@ -1,6 +1,6 @@
 const apiMap: Record<string, string> = {
   "2025-07": "https://script.google.com/macros/s/AKfycby-nn0mM3hJw5x_trEgl9ao6d6hFmwby9GA8Qcuk8zScrks3vsJThSLqoGu7EkN_UhXrQ/exec",
-  "2025-08": "https://script.google.com/macros/s/AKfycbxicxmOcsP18OTM2V1wIUpxGt4WWBG6kp5HYllPTMApK_UGVG2ZOIA4T18poTH3O8gUBA/exec",
+  "2025-08": "https://script.google.com/macros/s/AKfycbzIQL_D4aK1X4KciDkgQX-4I4Cy0bQKGKm_ie2fV4qfPSijjH4ON-T8TGHJXUL7z62-fQ/exec",
 };
 
 export function getTrackingApi(): string {
