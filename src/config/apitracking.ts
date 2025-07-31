ccfa4b64e27731dc073767cf06ad45b21b6e82fa
ariandto <92358210+ariandto@users.noreply.github.com>
2025-07-31 16:27:51 +0700

    up juli

--- src/config/apitracking.ts
+++ src/config/apitracking.ts
@@ -1,5 +1,5 @@
 const apiMap: Record<string, string> = {
-  "2025-07": "https://script.google.com/macros/s/AKfycboxOlaX9sXLv81EfWfOqULepP4HtrUCQF1e1vzai2h8c8TK34C62B9WhNy5X0YjdovPA/exec",
+  "2025-07": "https://script.google.com/macros/s/AKfycby-nn0mM3hJw5x_trEgl9ao6d6hFmwby9GA8Qcuk8zScrks3vsJThSLqoGu7EkN_UhXrQ/exec",
   "2025-08": "https://script.google.com/macros/s/AKfycbxicxmOcsP18OTM2V1wIUpxGt4WWBG6kp5HYllPTMApK_UGVG2ZOIA4T18poTH3O8gUBA/exec",
 };
 
