a6fff40a749088d82d7e818deb7ee4bcad70aeef
ariandto <92358210+ariandto@users.noreply.github.com>
2025-08-04 14:31:25 +0700

    fix bugs 1 app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// Layouts
 import Navigation from "./layouts/TopNavigation";
 import BottomNavigation from "./layouts/BottomNavigation";
+
+// Pages
 import Home from "./pages/Home";
 import UploadForm from "./pages/UploadForm";
 import Info from "./pages/Info";
@@ -10,21 +14,30 @@ import PicList from "./pages/PicList";
 import VisitorStats from "./pages/VisitorStats";
 import EditUserForm from "./pages/EditUserForm";
 
-
 function App() {
   return (
     <GoogleOAuthProvider clientId="459527241165-g48vjfufceg5havhol27af534ilir872.apps.googleusercontent.com">
       <Router>
         <Navigation />
         <Routes>
+          {/* Halaman Utama */}
           <Route path="/" element={<Home />} />
+
+          {/* Form Upload PIC Baru */}
           <Route path="/upload" element={<UploadForm />} />
+
+          {/* Daftar PIC */}
           <Route path="/listpic" element={<PicList />} />
           <Route path="/listpic/:divisi" element={<PicList />} />
+
+          {/* Halaman Tambahan */}
           <Route path="/info" element={<Info />} />
           <Route path="/form-request" element={<Form />} />
           <Route path="/visitor" element={<VisitorStats />} />
+
+          {/* Edit User */}
           <Route path="/update-user" element={<EditUserForm />} />
+          {/* Optional: dukung versi lama */}
           <Route path="/update-user/:id" element={<EditUserForm />} />
         </Routes>
         <BottomNavigation />
